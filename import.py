5143a91099fe732d69f7de8ae9ed680a22d987a7
Heather <a29533342@gmail.com>
2021-02-15 21:31:31 +0800

    add count

--- import.py
+++ import.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count + 1
 	N = input('請輸入數字：')
 	N = int(N)
 	if N == r:
 		print('恭喜你，你猜對了')
+		print('這是你猜的第', count,'次')
 		break
 	elif N > r:
 		print('太大了')
 	elif N < r:
 		print('太小了')
+	print('這是你猜的第', count,'次')
